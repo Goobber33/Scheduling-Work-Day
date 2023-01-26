3c663ac07f4ab2bff21ed4ab1efb12a8cb237ebb
Kyle Parks <kcparks1234@gmail.com>
2023-01-26 14:10:04 -0800

    Fixed issue where local storage was not saving on the page

--- Develop/JS/script.js
+++ Develop/JS/script.js
@@ -33,10 +33,10 @@ $(function () {
         if (pastTime === 0) {
             $(this).addClass('present');
         }
-        if (difference > 0) {
+        if (pastTime > 0) {
             $(this).addClass('future')
         }
-        if (difference < 0) {
+        if (pastTime < 0) {
             $(this).addClass('past');
         }
     });
@@ -46,16 +46,22 @@ $(function () {
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
 
-    $('time-block').each(function () {
+    $('.time-block').each(function () {
         var parent = $(this).attr("id");
         var setHour = parseInt(parent.slice[5]);
         var currentDay = dayjs().format('YYYY-MM-DD ' + setHour);
+        var currentHour = dayjs(currentDay);
+        var today = dayjs().format('YYYY-MM-DD H');
+        var pastTime = currentHour.diff(today, 'hour');
+        var work = localStorage.getItem(parent);
+        if (work != null) {
+            document.querySelector('#' + parent + ' textarea').value = work;
+        }
     });
 
     // TODO: Add code to display the current date in the header of the page.
+
+    var todaysDate = dayjs().format('dddd,MMMM D') + 'th';
+    $('#currentDay').text(todaysDate); //show current day on page top
+
 });
-
-
-
-var todaysDate = dayjs().format('dddd,MMMM D') + 'th';
-$('#currentDay').text(todaysDate); //show current day on page top
